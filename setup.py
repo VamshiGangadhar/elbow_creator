ba9d6fc301ace4a04b7f20b64c729fd0bac4e294
VamshiGangadhar <vamshi.gangadhar365@gmail.com>
2024-06-10 14:51:46 +0530

    removed meth from body

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'bpy',
-        'mathutils',
-        'math'
+        'mathutils'
     ],
     entry_points={
         'console_scripts': [
